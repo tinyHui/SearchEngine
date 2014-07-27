674d96bcb940103d56f13dee50a32df275bdef2f
Jason Chen <Jason_Chenjh@hotmail.com>
2014-07-27 21:54:19 +0800

    change thread pool size

--- app/Crawler/main.py
+++ app/Crawler/main.py
@@ -1,5 +1,5 @@
 from Downloader import Downloader
-from config import URL_DOWNLOAD_LIST, URL_VISITED_FILE_LIST, DOWLOAD_THREAD_POOL_SIZE
+from config import URL_DOWNLOAD_LIST, URL_VISITED_FILE_LIST, DOWLOAD_THREAD_POOL_SIZE, ANAYLIZER_THREAD_POOL_SIZE
 from BasicOperation import getBaseURL
 from HTMLAnaylizer.LinkExtractor import LinkExtractor
 from time import sleep
@@ -11,12 +11,20 @@ if __name__ == '__main__':
     # begin start multi threads
     URL_DOWNLOAD_LIST.put(start_url)
     thread_pool_download = []
+
+    ##################### create threads #####################
     for i in range(DOWLOAD_THREAD_POOL_SIZE):
-        new_thread_download = Downloader(thread_num=i)
-        thread_pool_download.append(new_thread_download)
-    
+        new_downloader = Downloader(thread_num=i)
+        thread_pool_download.append(new_downloader)
+
+    for i in range(ANAYLIZER_THREAD_POOL_SIZE):
+        new_link_extractor = LinkExtractor(base_url=base_url)
+    ##################### End #####################
+
+    ##################### start threads #####################
     for i in range(DOWLOAD_THREAD_POOL_SIZE):
         thread_pool_download[i].start()
 
-    thread_link_extract = LinkExtractor(base_url=base_url)
-    thread_link_extract.start()
+    for i in range(ANAYLIZER_THREAD_POOL_SIZE):
+        thread_link_extract.start()
+    ##################### End #####################
